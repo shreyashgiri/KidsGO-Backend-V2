09a3406b5708dea48612634f60aae00fc784b842
shreyashgiri <shreyashgiri0707@gmail.com>
2024-01-17 21:47:32 +0530

    path update

--- kidsgo/settings.py
+++ kidsgo/settings.py
@@ -97,9 +97,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATIC_ROOT = BASE_DIR /'staticfiles',
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / "media",
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
