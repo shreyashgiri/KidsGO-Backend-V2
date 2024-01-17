fe7b902af587a701affae03339922c227c28df20
shreyashgiri <shreyashgiri0707@gmail.com>
2024-01-17 21:26:48 +0530

    applied Secrete key

--- kidsgo/settings.py
+++ kidsgo/settings.py
@@ -9,11 +9,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'w13t@a1crgzy4d2ne%1ak+@^ao8())2ggzv)%pmm2n0p)lzfju'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'https://kidsgo.vercel.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
