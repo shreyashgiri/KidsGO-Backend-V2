fc12fa638257518b52c8e8af9adb9a2dc61bd634
shreyashgiri <shreyashgiri0707@gmail.com>
2024-01-17 21:44:33 +0530

    updated static

--- kidsgo/settings.py
+++ kidsgo/settings.py
@@ -96,7 +96,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
