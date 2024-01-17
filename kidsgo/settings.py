6413a85f94efa6d18a96f3a94f54bcd14295f09b
shreyashgiri <shreyashgiri0707@gmail.com>
2024-01-17 21:58:32 +0530

    removed trailing comma

--- kidsgo/settings.py
+++ kidsgo/settings.py
@@ -98,9 +98,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = BASE_DIR /'staticfiles',
+STATIC_ROOT = BASE_DIR /'staticfiles'
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media",
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
